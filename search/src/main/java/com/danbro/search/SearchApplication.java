78a57f2747d826adbe3585f94453f9d51fbb8c6e
Danbro007 <710170342@qq.com>
2021-02-15 20:31:18 +0800

    1、修改Search模块的配置文件

--- search/src/main/java/com/danbro/search/SearchApplication.java
+++ search/src/main/java/com/danbro/search/SearchApplication.java
@@ -3,6 +3,8 @@ package com.danbro.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Classname SearchApplication
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @Date 2021/2/15 16:20
  * @Created by Administrator
  */
+@EnableDiscoveryClient
+@EnableSwagger2
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SearchApplication {
     public static void main(String[] args) {
