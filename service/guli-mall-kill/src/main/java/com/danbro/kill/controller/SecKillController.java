80402b53d6b73da4d8691b046b71767f1600750c
Danbro007 <710170342@qq.com>
2021-03-18 21:58:40 +0800

    1添加秒杀成功的页面

--- service/guli-mall-kill/src/main/java/com/danbro/kill/controller/SecKillController.java
+++ service/guli-mall-kill/src/main/java/com/danbro/kill/controller/SecKillController.java
@@ -6,10 +6,12 @@ import com.danbro.kill.service.SecKillService;
 import com.danbro.kill.vo.SmsSeckillSkuRelationVo;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Danrbo
@@ -17,17 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description TODO
  * @Date 2021/3/17 12:56
  */
-@RestController
+@Controller
 public class SecKillController {
     @Autowired
     SecKillService secKillService;
 
+    @ResponseBody
     @ApiOperation("获取当前在秒杀的商品列表")
     @GetMapping("currentSecKillSkuList")
     public ResultBean<List<SmsSeckillSkuRelationVo>> getCurrentSecKillSkuList() {
         return ResultBean.ofSuccess(secKillService.getCurrentSecKillSkuList());
     }
 
+    @ResponseBody
     @ApiOperation("查询要秒杀商品的秒杀信息")
     @GetMapping("seckill/info/{skuId}")
     public ResultBean<SmsSeckillSkuRelationVo> getSecKillInfo(@PathVariable Long skuId) {
@@ -36,9 +40,12 @@ public class SecKillController {
 
     @ApiOperation("秒杀商品")
     @GetMapping("kill")
-    public ResultBean<String> killSku(@RequestParam("killId") String killId,
-                                      @RequestParam("randomCode") String randomCode,
-                                      @RequestParam("num") Integer num) {
-        return ResultBean.ofSuccess(secKillService.killSku(killId, randomCode, num));
+    public String killSku(@RequestParam("killId") String killId,
+                          @RequestParam("randomCode") String randomCode,
+                          @RequestParam("num") Integer num,
+                          Model model) {
+        String orderSn = secKillService.killSku(killId, randomCode, num);
+        model.addAttribute("orderSn", orderSn);
+        return "success";
     }
 }
