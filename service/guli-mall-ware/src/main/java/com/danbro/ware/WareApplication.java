8f693370a18f5646e309de3567f980608643c0bf
Danbro007 <710170342@qq.com>
2021-04-05 17:43:34 +0800

    完善秒杀功能

--- service/guli-mall-ware/src/main/java/com/danbro/ware/WareApplication.java
+++ service/guli-mall-ware/src/main/java/com/danbro/ware/WareApplication.java
@@ -3,6 +3,7 @@ package com.danbro.ware;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -19,7 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableSwagger2
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = "com.danbro")
 public class WareApplication {
